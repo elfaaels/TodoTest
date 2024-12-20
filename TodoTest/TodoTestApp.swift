b98ce1a793f1c8baba48972ede3f0b9f560a5f34
Elfana, A | Elfaael <elfana.engineer@gmail.com>
2024-12-20 09:06:22 +0700

    Initial Commit

---
+++ TodoTest/TodoTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoTestApp.swift
+//  TodoTest
+//
+//  Created by Elfana Anamta Chatya on 20/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
